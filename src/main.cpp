8fbbc571e3c6f1dc3d07185b14358725f14c3d4f
mankai0 <87978582+mankai0@users.noreply.github.com>
2024-11-27 14:48:23 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,14 @@ int main(){
     //aggiungo scansione
     std::vector<double>scan={1.01, 1.10, 1.65, 1.45, 1.3, 0.98, 0.9, 0.88, 0.75};
     lidar.new_scan(scan); 
+    std::vector<double>scan={1.01, 1.10, 1.65, 1.45, 1.3, 0.98, 0.9, 0.88, 0.75};
+    lidar.new_scan(scan); 
 
     //stampo ultima scansione
     std::cout<<"Ultima scansione: "<<lidar;
+
+    //Elimina prima scansione, e stampa i suoi valori.
+    lidar.get_scan();
 
     lidar.clear_buffer();
 
